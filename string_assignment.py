788306959a2273922c2ce4c1b6a55528ae375be7
Sajjad Ahmad <sajibaba05@gmail.com>
2025-05-14 06:27:51 +0500

    Add files via upload

---
+++ string_assignment.py
@@ -0,0 +1,40 @@
+# A Character is a single letter, digit, or symbol, 
+# like: a, 1, @, etc.
+char1 = "A"
+char2 = "9"
+char3 = "#"
+print(char1, char2, char3)
+
+# A String is a collection of characters, words, or sentences enclosed in quotes.
+animal="Tiger"
+sentence="Tigers are strong"
+print(animal)
+print(sentence)
+
+# Joining Strings
+
+first_name="sajjad"
+mid_name="ahmad"
+last_name="awan"
+full_name=first_name+" "+ mid_name+" "+last_name
+print(full_name)
+
+# Repitation
+
+greeting= "Hi"
+print(greeting*5)
+
+# Indexing
+
+phrase="PythonRocks"
+print(phrase [0])
+print(phrase [3])
+print(phrase [-1])
+
+# Slicing
+
+ex="I love coding in Python"
+print(ex [2:6])
+print(len(ex))
+print(ex [17:23])
+
